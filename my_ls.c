95f713054af3570e5a9ed3d97db72cce72889b74
WanderT444 <111989477+WanderT444@users.noreply.github.com>
2024-05-10 15:02:05 -0400

    a headers, get cwd + arg handling

---
+++ my_ls.c
@@ -0,0 +1,31 @@
+// File will contain a trimmed down or lite if you will recreation of the linux LS command
+// The program can be ran with no argumenets or a singular arguement 
+// Last Edited by Wander Cerda Torres on 05/  /2024
+
+
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main(int argc, const char * argv[]) {
+
+char cwd[4096];
+
+// for checking the current working directory 
+getcwd(cwd, sizeof(cwd));
+    
+    if (getcwd(cwd, sizeof(cwd)) == NULL){
+        perror("Fatal Error");
+        exit(1);
+    }
+
+   if (argc > 2) {
+    printf("%s"," Error too many arguements My_LS can only handle 1 arguement at a time\n");
+    return(1);
+   }
+
+} // end main 
